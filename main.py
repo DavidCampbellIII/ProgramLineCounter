cb41e4dc1c237a0ec5c52d8b4dc39cca337f9c5d
David Campbell III <lapidistcubed@gmail.com>
2020-08-06 21:59:42 -0500

    Added redo and ability to ignore a pattern

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def on_browse_click():
         btn_start_count.pack()
 
 
+def on_ignore_patterns_select():
+    if ignore_patterns.get():
+        etr_ignore_patterns.pack()
+    else:
+        etr_ignore_patterns.pack_forget()
+
 def start_count():
     root_dir = lbl_current_path["text"]
     lbl_instructions["text"] = "Starting search from root file '" + root_dir + "'"
@@ -22,22 +28,27 @@ def start_count():
     lbl_current_path.pack_forget()
     chk_ignore_blank_lines.pack_forget()
     chk_ignore_bracket_lines.pack_forget()
+    chk_ignore_patterns.pack_forget()
 
     txt_status_box.pack()
-    file_extension = etr_file_extension.get()
+    file_extension = etr_file_extension.get().strip()
+    if file_extension[0] != ".": #add the '.' if it is not already there
+        file_extension = "." + file_extension
+
     total_line_count = 0
     for subdir, _, files in os.walk(root_dir):
         for file in files:
             filepath = subdir + os.sep + file
             update_status("Analyzing " + filepath + "...")
-            #time.sleep(MILLISECOND) #TODO how do you properly wait so the status box is updated as the program processes the data?
+            #time.sleep(MILLISECOND * 500) #TODO how do you properly wait so the status box is updated as the program processes the data?
             if filepath.endswith(file_extension):
                 with open(filepath) as f:
                     length = count_lines(f.readlines())
                     update_status("Found " + str(length) + (" lines!\n", " line!\n")[length == 1])
                     total_line_count += length
     
-    update_status("Total lines: " + str(total_line_count))
+    update_status("\nDONE!\n\nTotal lines: " + str(total_line_count))
+    btn_redo.pack()
 
 
 def count_lines(lines):
@@ -45,7 +56,7 @@ def count_lines(lines):
     for line in lines:
         line = line.strip()
         if len(line) == 0:
-            if not ignore_blank_lines.get():
+            if not ignore_bracket_lines.get():
                 count += 1
         elif len(line) == 1:
             if any((c in brackets) for c in line):
@@ -57,9 +68,26 @@ def count_lines(lines):
             count += 1
     return count
 
+
 def update_status(msg):
     txt_status_box.insert(tk.END, msg + "\n")
     txt_status_box.see("end")
+
+def set_up_main():
+    txt_status_box.pack_forget()
+    btn_redo.pack_forget()
+
+    frm_header.pack()
+    lbl_instructions.pack()
+    btn_browse.pack()
+    etr_file_extension.pack()
+    chk_ignore_blank_lines.pack()
+    chk_ignore_bracket_lines.pack()
+    chk_ignore_patterns.pack()
+    lbl_current_path.pack()
+
+    lbl_current_path["text"] = "None"
+    txt_status_box.delete(1.0, tk.END)
     
 
 #=======================Window Setup=========================
@@ -80,17 +108,16 @@ chk_ignore_bracket_lines = tk.Checkbutton(text="Ignore Bracket Lines", variable=
 etr_file_extension = tk.Entry()
 etr_file_extension.insert(0, ".cs")
 
+ignore_patterns = tk.IntVar()
+chk_ignore_patterns = tk.Checkbutton(text="Ignore pattern", variable=ignore_patterns, onvalue=True, offvalue=False, command=on_ignore_patterns_select)
+etr_ignore_patterns = tk.Entry()
+
 lbl_current_path = tk.Label(text="None", fg='#03bafc')
 
 txt_status_box = tk.Text()
 txt_status_box.bind("<Key>", lambda e: "break") #makes the status box readonly
+btn_redo = tk.Button(text="Redo", command=set_up_main)
 
-frm_header.pack()
-lbl_instructions.pack()
-btn_browse.pack()
-etr_file_extension.pack()
-chk_ignore_blank_lines.pack()
-chk_ignore_bracket_lines.pack()
-lbl_current_path.pack()
+set_up_main()
 
 window.mainloop()
