15ffe27c944593888e34a6cac3b13d6e8712c921
David Campbell III <lapidistcubed@gmail.com>
2021-06-10 14:14:13 -0500

    refactored and started to add specific language support

---
+++ language.py
@@ -0,0 +1,10 @@
+class Language:
+    def __init__(self, name, extension, comment, canHaveMultiLines, lineEnder):
+        self.name = name
+        self.extension = extension
+        self.comment = comment
+        self.canHaveMultiLines = canHaveMultiLines
+        self.lineEnder = lineEnder
+
+    def loadLanguages(path):
+        pass
---
+++ tests/test.cs
@@ -0,0 +1,13 @@
+//Ignoring:
+//Comments - 8 lines
+//Comments and brackets - 4 lines
+//Comments, brackets, and multilines as one - 3 lines
+class Test
+{
+    //this is a test comment in C#
+    public static void main(string[] args)
+    {
+        Console.WriteLine("Here is a " +
+    "multi line chunk of code");
+    }
+}
